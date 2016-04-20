c4ac696b76132e58679679aa46b8e27568549276
Sebastian Kromka <Sebastian Kromka>
2016-04-20 13:16:02 +0200

    add printDepth()

--- Output.h
+++ Output.h
@@ -1,10 +1,22 @@
 #ifndef _OUTPUT_H_
 #define _OUTPUT_H_
 
-void DrawBoard(int board[board_size][board_size]) {
+void printDepth(int depth) {
+	printf("\n");
+	for (int i = 0; i < depth; i++) {
+		printf("   ");
+	}
+}
+
+void drawBoard(int board[board_size][board_size], int depth) {
 	printf("\n");
 	for (int x = 0; x < board_size; x++) {
 		for (int y = 0; y < board_size; y++) {
+			if (y == 0) {
+				for (int i = 0; i < depth; i++) {
+					printf("   ");
+				}
+			}
 			printf("%d", board[x][y]);
 			if (y != board_size - 1) {
 				printf("-");
@@ -15,6 +27,11 @@ void DrawBoard(int board[board_size][board_size]) {
 		}
 		if (x < board_size - 1) {
 			for (int y = 0; ; y++) {
+				if (y == 0) {
+					for (int i = 0; i < depth; i++) {
+						printf("   ");
+					}
+				}
 				if (y < board_size - 1) {
 					printf("|/");
 				}
