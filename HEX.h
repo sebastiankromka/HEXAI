acb25740cb31c3e439bd24e2788e447a3e3a9e39
Sebastian Kromka <Sebastian Kromka>
2016-06-04 10:43:08 +0200

    add game AI2 vs human

--- HEX.h
+++ HEX.h
@@ -11,14 +11,14 @@ int getOpponent(int player) {
 	}
 }
 
-void moveHuman(int board[boardSize][boardSize]) {
+void moveHuman(int board[boardSize][boardSize], int symbol) {
 	int x;
 	int y;
 	do {
 		scanf("%d", &x);
 		scanf("%d", &y);
 	} while (x >= boardSize || y >= boardSize || board[x][y] != free);
-	board[x][y] = AI2;
+	board[x][y] = symbol;
 }
 
 void gameAI1vsHuman(int awardsAI1[numberOfAwards], int firstPlayer, int logLevel) {
@@ -39,7 +39,7 @@ void gameAI1vsHuman(int awardsAI1[numberOfAwards], int firstPlayer, int logLevel
 			moveAI(AI1, board, awardsAI1, logLevel);
 		}
 		else {
-			moveHuman(board);
+			moveHuman(board, AI2);
 		}
 		state = checkState(board);
 		drawBoard(board);
@@ -51,7 +51,37 @@ void gameAI1vsHuman(int awardsAI1[numberOfAwards], int firstPlayer, int logLevel
 	getchar();
 }
 
-int gameAI1vsAI2(int awardsAI1[numberOfAwards], int awardsAI2[numberOfAwards], int firstPlayer) {
+void gameAI2vsHuman(int awardsAI1[numberOfAwards], int firstPlayer, int logLevel) {
+	printf("start game");
+
+	int currentPlayer = firstPlayer;
+	int state;
+	int board[boardSize][boardSize] = { { 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0 },
+	{ 0, 0, 0, 0, 0 } };
+	drawBoard(board);
+
+	// game loop
+	do {
+		if (currentPlayer == AI2) {
+			moveAI(AI2, board, awardsAI1, logLevel);
+		}
+		else {
+			moveHuman(board, AI1);
+		}
+		state = checkState(board);
+		drawBoard(board);
+		currentPlayer = getOpponent(currentPlayer);
+	} while (state == noWinner);
+
+	printf("\nEND GAME state = %d\nPress enter to continue...\n", state);
+	getchar();
+	getchar();
+}
+
+int gameAI1vsAI2(int awardsAI1[numberOfAwards], int awardsAI2[numberOfAwards], int firstPlayer, int logLevel) {
 	int currentPlayer = firstPlayer;
 	int state;
 	int result = 40;
@@ -64,16 +94,22 @@ int gameAI1vsAI2(int awardsAI1[numberOfAwards], int awardsAI2[numberOfAwards], i
 	do {
 		result--;
 		if (currentPlayer == AI1) {
-			moveAI(AI1, board, awardsAI1, 0);
+			moveAI(AI1, board, awardsAI1, logLevel);
 		}
 		else {
-			moveAI(AI2, board, awardsAI2, 0);
+			moveAI(AI2, board, awardsAI2, logLevel);
 		}
 		currentPlayer = getOpponent(currentPlayer);
+		if (logLevel > 0) {
+			drawBoard(board);
+		}
 		state = checkState(board);
 	} while (state == noWinner);
 	if (state == AI2) {
 		result = -result;
+	}
+	if (logLevel > 0) {
+		printf("\nEND GAME state = %d\n", state);
 	}
 	return result;
 }
